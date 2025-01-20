979b447c073ecf672dd280c4c9ef93042d64acee
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-01-21 00:45:07 +0300

    feat: raw term input

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,18 @@
 const std = @import("std");
+const term = @import("term.zig");
 const io = std.io;
 
 pub fn main() !void {
-    const reader = io.getStdIn().reader();
-    const writer = io.getStdOut().writer();
+    const stdin = io.getStdIn();
+    const stdout = io.getStdOut();
+
+    const reader = stdin.reader();
+    const writer = stdout.writer();
 
     try writer.print("Welcome to Benihime\n", .{});
+
+    var raw_term = try term.enableRawMode(stdin.handle);
+    defer raw_term.disableRawMode() catch {};
 
     while (true) {
         const input = try reader.readByte();
