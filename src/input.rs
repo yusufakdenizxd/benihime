477554a183b917ee71b210fd5f6e24bc470e5885
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-09-07 20:52:38 +0200

    from crossterm keycodes to my implementation

--- src/input.rs
+++ src/input.rs
@@ -1,9 +1,83 @@
-use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{KeyEvent, KeyModifiers};
 
 use crate::editor::{Editor, Mode};
 
-pub fn handle_normal(ed: &mut Editor, key: KeyEvent) {
-    match key.code {
+#[derive(Debug, PartialOrd, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum ModifierKeyCode {
+    LeftShift,
+    LeftControl,
+    LeftAlt,
+    LeftSuper,
+    LeftHyper,
+    LeftMeta,
+    RightShift,
+    RightControl,
+    RightAlt,
+    RightSuper,
+    RightHyper,
+    RightMeta,
+    IsoLevel3Shift,
+    IsoLevel5Shift,
+}
+
+#[derive(Debug, PartialOrd, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum KeyCode {
+    Backspace,
+    Enter,
+    Left,
+    Right,
+    Up,
+    Down,
+    Tab,
+    BackTab,
+    Delete,
+    Insert,
+    /// F key.
+    F(u8),
+    Char(char),
+    Null,
+    Esc,
+    Modifier(ModifierKeyCode),
+}
+
+impl KeyCode {
+    pub fn form_crossterm(k: crossterm::event::KeyCode) -> KeyCode {
+        if k.is_esc() {
+            return KeyCode::Esc;
+        }
+
+        if k.is_backspace() {
+            return KeyCode::Backspace;
+        }
+
+        if let Some(char) = k.as_char() {
+            return KeyCode::Char(char);
+        }
+        return KeyCode::Null;
+    }
+
+    pub fn is_function_key(&self, n: u8) -> bool {
+        matches!(self, KeyCode::F(m) if *m == n)
+    }
+
+    pub fn is_char(&self, c: char) -> bool {
+        matches!(self, KeyCode::Char(m) if *m == c)
+    }
+
+    pub fn as_char(&self) -> Option<char> {
+        match self {
+            KeyCode::Char(c) => Some(*c),
+            _ => None,
+        }
+    }
+
+    pub fn is_modifier(&self, modifier: ModifierKeyCode) -> bool {
+        matches!(self, KeyCode::Modifier(m) if *m == modifier)
+    }
+}
+
+pub fn handle_normal(ed: &mut Editor, k: KeyEvent) {
+    match KeyCode::form_crossterm(k.code) {
         KeyCode::Char('h') => ed.move_left(),
         KeyCode::Char('j') => ed.move_down(),
         KeyCode::Char('k') => ed.move_up(),
@@ -37,7 +111,7 @@ pub fn handle_normal(ed: &mut Editor, key: KeyEvent) {
 }
 
 pub fn handle_insert(ed: &mut Editor, key: KeyEvent) {
-    match key.code {
+    match KeyCode::form_crossterm(key.code) {
         KeyCode::Esc => ed.mode = Mode::Normal,
         KeyCode::Backspace => {
             if ed.cursor.col > 0 {
