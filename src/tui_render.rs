f622e6b84c9335b9f10cee3c50a3439fcf45285c
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-09-09 21:11:39 +0200

    clean main function

--- src/tui_render.rs
+++ src/tui_render.rs
@@ -8,7 +8,74 @@ use crossterm::{
 use std::cmp::min;
 use std::io::Write;
 
+use crossterm::ExecutableCommand;
+use crossterm::cursor::Hide;
+use crossterm::event::{
+    DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers, read,
+};
+use crossterm::terminal::{
+    EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+};
+use std::io::stdout;
+
 use crate::editor::{Editor, Mode};
+use crate::input;
+
+pub fn run() -> Result<()> {
+    let mut ed = Editor::with_text(
+        "Hello, Vim‑ish world!\nThis is a demo buffer.\nUse hjkl, wbe, 0,$, gg/G, i/a/o/O, x, dd, yy, p, u, Ctrl‑r.",
+    );
+
+    enable_raw_mode()?;
+    let mut out = stdout();
+    out.execute(EnterAlternateScreen)?;
+    out.execute(EnableMouseCapture)?;
+    out.execute(Hide)?;
+
+    let result = (|| -> Result<()> {
+        loop {
+            render(&mut ed)?;
+            match read()? {
+                Event::Key(key) => {
+                    if key.code == KeyCode::Esc
+                        && ed.mode == Mode::Normal
+                        && key.modifiers.contains(KeyModifiers::SHIFT)
+                    {
+                        break;
+                    }
+                    if key.code == KeyCode::Char('c')
+                        && key.modifiers.contains(KeyModifiers::CONTROL)
+                    {
+                        break;
+                    }
+
+                    match ed.mode {
+                        Mode::Normal => input::handle_normal(&mut ed, key),
+                        Mode::Insert => input::handle_insert(&mut ed, key),
+                        Mode::Visual => {
+                            if let KeyCode::Esc = key.code {
+                                ed.mode = Mode::Normal;
+                            } else { /* TODO: selections */
+                            }
+                        }
+                    }
+                }
+                Event::Resize(_, _) => {}
+                _ => {}
+            }
+        }
+        Ok(())
+    })();
+
+    disable_raw_mode()?;
+    out.execute(SetCursorStyle::DefaultUserShape)?;
+    out.execute(Show)?;
+    out.execute(DisableMouseCapture)?;
+    out.execute(LeaveAlternateScreen)?;
+
+    result
+}
+
 pub fn render(editor: &mut Editor) -> Result<bool> {
     let (w, h) = size()?;
     editor.ensure_cursor_on_screen(w, h);
