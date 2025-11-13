fdb217a583f771829e93383690a942cf46be29d7
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-11-13 17:14:43 +0100

    refactor: move render to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use render::gui_render;
+
 mod buffer;
 mod buffer_manager;
 mod command;
 mod editor;
-mod gui_render;
 mod keymap;
 mod mini_buffer;
 mod motion;
+mod render;
 
 fn main() {
     let _ = gui_render::run();
