8ac212afd5782e61a484a3709c396a5228a10092
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-12-21 17:08:34 +0100

    feat: cmd types

--- src/command/command_registry.rs
+++ src/command/command_registry.rs
@@ -3,10 +3,10 @@ use std::collections::HashMap;
 
 use crate::editor::HandleKeyError;
 
-use super::command::{CommandArg, CommandContext, CommandFn};
+use super::command::{Cmd, CommandArg, CommandContext, CommandFn};
 
 pub struct CommandRegistry {
-    pub commands: HashMap<String, CommandFn>,
+    pub commands: HashMap<String, Cmd>,
 }
 
 impl CommandRegistry {
@@ -17,37 +17,74 @@ impl CommandRegistry {
     }
 
     pub fn register(&mut self, name: &str, cmd: CommandFn) {
-        self.commands.insert(name.to_string(), cmd);
+        self.commands.insert(name.to_string(), Cmd::Plain(cmd));
+    }
+
+    pub fn register_motion(&mut self, name: &str, cmd: CommandFn) {
+        self.commands.insert(name.to_string(), Cmd::Motion(cmd));
+    }
+
+    pub fn register_operator(&mut self, name: &str, cmd: CommandFn) {
+        self.commands.insert(name.to_string(), Cmd::Operator(cmd));
+    }
+
+    pub fn register_navigation(&mut self, name: &str, cmd: CommandFn) {
+        self.commands.insert(name.to_string(), Cmd::Navigation(cmd));
+    }
+
+    pub fn register_system(&mut self, name: &str, cmd: CommandFn) {
+        self.commands.insert(name.to_string(), Cmd::System(cmd));
     }
 
     pub fn execute(&self, name: &str, ctx: &mut CommandContext) -> Result<(), HandleKeyError> {
         if let Some(cmd) = self.commands.get(name) {
-            match cmd(ctx) {
-                Ok(o) => {
-                    if name != "clear-error-message" && name != "error-message" {
-                        let _ = self.execute(
-                            "clear-error-message",
-                            &mut CommandContext {
-                                state: ctx.state,
-                                args: &None,
-                            },
-                        );
-                    }
-                    Ok(o)
+            match cmd {
+                Cmd::Plain(f) => {
+                    ctx.state.focused_buf_mut().range = None;
+                    self.call_cmd(f, ctx, name)
                 }
-                Err(e) => {
-                    let _ = self.execute(
-                        "error-message",
-                        &mut CommandContext {
-                            state: ctx.state,
-                            args: &Some(vec![CommandArg::Str(e.to_string())]),
-                        },
-                    );
-                    Err(HandleKeyError::ExecutionFailed(e))
+                Cmd::Motion(f) => self.call_cmd(f, ctx, name),
+                Cmd::Operator(f) => self.call_cmd(f, ctx, name),
+                Cmd::System(f) => self.call_cmd(f, ctx, name),
+                Cmd::Navigation(f) => {
+                    ctx.state.focused_buf_mut().range = None;
+                    self.call_cmd(f, ctx, name)
                 }
             }
         } else {
             Err(HandleKeyError::CommandNotFound(name.to_string()))
+        }
+    }
+
+    fn call_cmd(
+        &self,
+        f: &CommandFn,
+        ctx: &mut CommandContext,
+        name: &str,
+    ) -> Result<(), HandleKeyError> {
+        match f(ctx) {
+            Ok(o) => {
+                if name != "clear-error-message" && name != "error-message" {
+                    let _ = self.execute(
+                        "clear-error-message",
+                        &mut CommandContext {
+                            state: ctx.state,
+                            args: &None,
+                        },
+                    );
+                }
+                Ok(o)
+            }
+            Err(e) => {
+                let _ = self.execute(
+                    "error-message",
+                    &mut CommandContext {
+                        state: ctx.state,
+                        args: &Some(vec![CommandArg::Str(e.to_string())]),
+                    },
+                );
+                Err(HandleKeyError::ExecutionFailed(e))
+            }
         }
     }
 }
