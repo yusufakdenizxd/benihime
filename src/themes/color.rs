3d88d127ad6c7d3f68f7dceafd457ec7165cc0ee
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-12-24 19:28:51 +0100

    feat: color

---
+++ src/themes/color.rs
@@ -0,0 +1,17 @@
+#[derive(Clone, Copy, Debug)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub const fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b, a: 255 }
+    }
+
+    pub const fn from_rgba(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self { r, g, b, a }
+    }
+}
