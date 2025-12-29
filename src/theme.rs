96e89e7933b963942d247d2647d760b11922917a
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-12-29 17:58:21 +0100

    refactor: color default

--- src/theme.rs
+++ src/theme.rs
@@ -6,23 +6,12 @@ use bitflags::bitflags;
 
 use crate::hashmap;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
     pub a: u8,
-}
-
-impl Default for Color {
-    fn default() -> Self {
-        Color {
-            r: 0,
-            g: 0,
-            b: 0,
-            a: 0,
-        }
-    }
 }
 
 impl Color {
