94d4b5ad91d3c8a95fb999a8c0d3a88abcd328e0
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2026-01-20 15:56:43 +0100

    refactor: remove optional value from current project since it has default value

--- benihime-core/src/render/statusline.rs
+++ benihime-core/src/render/statusline.rs
@@ -7,9 +7,7 @@ pub fn render_statusline(ctx: &Context, state: &EditorState) {
         ui.horizontal(|ui| {
             ui.label(state.status_line());
             ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
-                if let Some(name) = state.project_manager.current_name() {
-                    ui.label(name);
-                }
+                ui.label(state.project_manager.current_name());
                 if state.focused_buf().mode == Mode::Command {
                     ui.label(format!("{}:", state.command_buffer));
                 } else if state.error_message.is_some() {
