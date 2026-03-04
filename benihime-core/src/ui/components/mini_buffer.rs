c7d2acbdefa542f993beb2814813281e34890128
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2026-03-04 08:09:49 +0100

    fix: apply masking to minibuffer items

--- benihime-core/src/ui/components/mini_buffer.rs
+++ benihime-core/src/ui/components/mini_buffer.rs
@@ -57,50 +57,58 @@ impl Component for MiniBufferComponent {
             let candidate_area_height = max_visible as u16 * cell_height;
             let candidate_area_start = input_y.saturating_sub(candidate_area_height);
 
-            surface.draw_rect(
+            surface.with_overlay_region(
                 area.x as f32,
                 candidate_area_start as f32,
                 area.width as f32,
                 candidate_area_height as f32,
-                benihime_renderer::color::Color::rgb(0.1, 0.1, 0.15),
+                |f| {
+                    f.draw_rect(
+                        area.x as f32,
+                        candidate_area_start as f32,
+                        area.width as f32,
+                        candidate_area_height as f32,
+                        benihime_renderer::color::Color::rgb(0.1, 0.1, 0.15),
+                    );
+
+                    for i in 0..max_visible {
+                        let candidate_idx = offset + i;
+                        if candidate_idx >= total_candidates {
+                            break;
+                        }
+
+                        let candidate_y = candidate_area_start + (i as u16 * cell_height);
+                        let candidate_text = &candidates[candidate_idx];
+
+                        let color = if candidate_idx == index {
+                            benihime_renderer::color::Color::rgb(0.9, 0.6, 0.4)
+                        } else {
+                            benihime_renderer::color::Color::rgb(0.6, 0.6, 0.6)
+                        };
+
+                        let section = benihime_renderer::text::TextSection::simple(
+                            area.x as f32,
+                            candidate_y as f32,
+                            candidate_text.as_str(),
+                            f.font_size(),
+                            color,
+                        );
+                        f.draw_text(section);
+                    }
+
+                    if total_candidates > max_visible {
+                        let scroll_info = format!("[{}/{}]", index + 1, total_candidates);
+                        let scroll_section = benihime_renderer::text::TextSection::simple(
+                            (area.x + area.width - 10) as f32,
+                            candidate_area_start as f32,
+                            scroll_info.as_str(),
+                            f.font_size(),
+                            benihime_renderer::color::Color::rgb(0.4, 0.4, 0.4),
+                        );
+                        f.draw_text(scroll_section);
+                    }
+                },
             );
-
-            for i in 0..max_visible {
-                let candidate_idx = offset + i;
-                if candidate_idx >= total_candidates {
-                    break;
-                }
-
-                let candidate_y = candidate_area_start + (i as u16 * cell_height);
-                let candidate_text = &candidates[candidate_idx];
-
-                let color = if candidate_idx == index {
-                    benihime_renderer::color::Color::rgb(0.9, 0.6, 0.4)
-                } else {
-                    benihime_renderer::color::Color::rgb(0.6, 0.6, 0.6)
-                };
-
-                let section = benihime_renderer::text::TextSection::simple(
-                    area.x as f32,
-                    candidate_y as f32,
-                    candidate_text.as_str(),
-                    surface.font_size(),
-                    color,
-                );
-                surface.draw_text(section);
-            }
-
-            if total_candidates > max_visible {
-                let scroll_info = format!("[{}/{}]", index + 1, total_candidates);
-                let scroll_section = benihime_renderer::text::TextSection::simple(
-                    (area.x + area.width - 10) as f32,
-                    candidate_area_start as f32,
-                    scroll_info.as_str(),
-                    surface.font_size(),
-                    benihime_renderer::color::Color::rgb(0.4, 0.4, 0.4),
-                );
-                surface.draw_text(scroll_section);
-            }
         }
 
         surface.draw_rect(
@@ -123,7 +131,7 @@ impl Component for MiniBufferComponent {
         if !input.is_empty() {
             let prompt_width = prompt.len() as f32 * surface.cell_width();
             let input_section = benihime_renderer::text::TextSection::simple(
-                area.x as f32 + prompt_width ,
+                area.x as f32 + prompt_width,
                 input_y as f32,
                 input,
                 surface.font_size(),
