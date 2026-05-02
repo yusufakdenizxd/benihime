f81622b1cf2ff6c8dba1d8add332effa2eaa83e7
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2026-05-02 23:40:38 +0200

    fix: show error in statusline

--- benihime-core/src/ui/components/status_line.rs
+++ benihime-core/src/ui/components/status_line.rs
@@ -55,5 +55,18 @@ impl Component for StatusLine {
             );
             surface.draw_text(section);
         }
+
+        if let Some(ref err) = editor.error_message {
+            let err_width = (err.len() as f32 + 1.0) * surface.cell_width();
+            let x = area.x as f32 + area.width as f32 - err_width;
+            let section = benihime_renderer::text::TextSection::simple(
+                x,
+                y as f32,
+                err.clone(),
+                surface.font_size(),
+                benihime_renderer::color::Color::rgb(1.0, 0.3, 0.3),
+            );
+            surface.draw_text(section);
+        }
     }
 }
